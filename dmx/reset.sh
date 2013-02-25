0883bdfb35d32bddd0d48b452b54f7f23fd29ec2
Zachary Kotlarek <zach@kotlarek.com>
2013-02-24 23:42:35 -0800

    Reset the fan controller now that it subscribes to state

--- dmx/reset.sh
+++ dmx/reset.sh
@@ -2,8 +2,9 @@
 
 ROPE="`ps -A -o pid=,command= | grep -v grep | grep rope.pl | awk '{print $1}'`"
 LEDS="`ps -A -o pid=,command= | grep -v grep | grep leds.pl | awk '{print $1}'`"
+FAN="`ps -A -o pid=,command= | grep -v grep | grep fan.pl | awk '{print $1}'`"
 
-for i in $ROPE $LEDS; do
+for i in $ROPE $LEDS $FAN; do
 	if [ -n "${i}" ]; then
 		kill $i
 	fi
