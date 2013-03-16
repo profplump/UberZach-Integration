463d79340cf387216be3a88882407a7424d30a7c
Zachary Kotlarek <zach@kotlarek.com>
2013-03-16 04:35:57 -0700

    Simplier reset

--- dmx/reset.sh
+++ dmx/reset.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-ROPE="`ps -A -o pid=,command= | grep -v grep | grep rope.pl | awk '{print $1}'`"
-LEDS="`ps -A -o pid=,command= | grep -v grep | grep leds.pl | awk '{print $1}'`"
-BIAS="`ps -A -o pid=,command= | grep -v grep | grep bias.pl | awk '{print $1}'`"
-PROJ="`ps -A -o pid=,command= | grep -v grep | grep projectorPower.pl | awk '{print $1}'`"
-FAN="`ps -A -o pid=,command= | grep -v grep | grep fan.pl | awk '{print $1}'`"
-AMP="`ps -A -o pid=,command= | grep -v grep | grep ampliferPower.pl | awk '{print $1}'`"
-OVERHEAD="`ps -A -o pid=,command= | grep -v grep | grep overhead.pl | awk '{print $1}'`"
-GARAGE="`ps -A -o pid=,command= | grep -v grep | grep garage.pl | awk '{print $1}'`"
+PROCS="rope.pl leds.pl bias.pl fan.pl overhead.pl projectorPower.pl amplifierPower.pl garage.pl"
 
-for i in $ROPE $LEDS $FAN $BIAS $OVERHEAD $GARAGE $AMP $PROJ; do
-	if [ -n "${i}" ]; then
-		kill $i
-	fi
+# Find the PID for each named process
+PIDS=""
+for i in $PROCS; do
+	PID="`ps -A -o pid=,command= | grep -v grep | grep ${i} | awk '{print $1}'`"
+	PIDS="${PIDS} ${PID}"
 done
+
+# Kill everything we found
+kill $PIDS
