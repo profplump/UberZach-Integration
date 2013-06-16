2fc22c8c9895fe979003f0d170bb000913d094ea
Zachary Kotlarek <zach@kotlarek.com>
2013-06-16 02:57:24 -0700

    Launch the Plex client and mount media at login

---
+++ plexMonitor/plexHelper.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Wait for the media share to be mounted
+if ! ~/bin/video/isMediaMounted; then
+	~/bin/video/mountMedia
+	sleep 5
+	exit 1
+fi
+
+# Kill previous instances
+killall Plex
+
+# Exec
+exec '/Applications/Zach/Media/Plex.app/Contents/MacOS/Plex'
