e9047a8922d90e7504fd70254bb96076eb0574b2
Zachary Kotlarek <zach@kotlarek.com>
2013-11-03 01:33:08 -0700

    Also TRUE when optimizing

--- pms/isScanning.sh
+++ pms/isScanning.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Return true if scanning
 if ps auwx | grep -v grep | grep -q 'Plex Media Scanner'; then
 	exit 0
 fi
+
+# Return true if optimizing
+if ps auwx | grep -v grep | grep curl | grep -q 'upload-file'; then
+	exit 0
+fi
+
+# Otherwise return false
 exit 1
