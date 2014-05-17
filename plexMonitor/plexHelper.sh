ae3b19f56e4d5ce5ad25e20810e7c1a3a8db135f
Zachary Kotlarek <zach@kotlarek.com>
2014-05-16 18:26:52 -0700

    Always attempt a re-mount, in case the network changes

--- plexMonitor/plexHelper.sh
+++ plexMonitor/plexHelper.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+# Always re-trigger a mount (to fix links if the network changes)
+~/bin/video/mountMedia
+
 # Wait for the media share to be mounted
 if ! ~/bin/video/isMediaMounted; then
-	~/bin/video/mountMedia
 	sleep 5
 	exit 1
 fi
